b47a8533710cdd850670e877086f54ce0cdac526
Matthew Kenny <matthewkenny@gmail.com>
2012-12-19 09:58:34 -0800

    group_mapcode spaghetti!

--- static/js/group_mapcode.js
+++ static/js/group_mapcode.js
@@ -4,10 +4,58 @@
  * Date: 12/18/12
  * Time: 9:37 PM
  * To change this template use File | Settings | File Templates.
+ * 47.6097° N, 122.3331° W
  */
-var map = L.map('map').setView([51.505, -0.09], 13);
+var geoJsonBaseURL = "http://127.0.0.1:8000/GPSTracker/geojson/";
 
-L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+var pointGroupURL = geoJsonBaseURL + "point/group/" + group_id;
+var lineGroupURL = geoJsonBaseURL + "line/group/" + group_id;
+var polyGroupURL = geoJsonBaseURL + "poly/group/" + group_id;
+
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+var map = L.map('map').setView([0,0], 1);
+
+var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/5df4a1ed4a8c41bd91725ad594aa6139/997/256/{z}/{x}/{y}.png'
+var cloudmadeAttrib = 'Map data &copy; 2011 OSM contributors, Style &copy; 2011 CloudMade'
+var cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttrib});
+
+var mapQuestAerialURL = 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png'
+var mapQuestAerialAttrib = 'Portions Courtesy NASA/JPL-CalTech, USDA FSA. Thanks MapQuest!'
+var mapQuestAerial = new L.TileLayer(mapQuestAerialURL, {
     maxZoom: 18,
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
-}).addTo(map);
+    attribution: mapQuestAerialAttrib,
+    subdomains: ['oatile1','oatile2','oatile3','oatile4']
+});
+map.addLayer(mapQuestAerial);
+
+var baseMaps = {"Road": cloudmade, "Aerial": mapQuestAerial};
+var pointGeoJSON = new L.GeoJSON();
+var lineGeoJSON = new L.GeoJSON();
+var polyGeoJSON = new L.GeoJSON();
+var layersControl = new L.Control.Layers(baseMaps);
+map.addControl(layersControl);
+
+// Make call to GeoJSON Service
+$.getJSON(pointGroupURL, makePointLayer);
+$.getJSON(lineGroupURL, makeLineLayer);
+$.getJSON(polyGroupURL, makePolyLayer);
+
+function makePointLayer(data){
+    pointGeoJSON.addData(data);
+    pointGeoJSON.addTo(map)
+}
+
+function makeLineLayer(data){
+    lineGeoJSON.addData(data);
+    lineGeoJSON.addTo(map)
+}
+
+function makePolyLayer(data){
+    polyGeoJSON.addData(data);
+    polyGeoJSON.addTo(map)
+}
