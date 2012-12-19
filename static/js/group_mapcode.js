53c4287e0ccc2d878dc4aa15183d8e93f6563438
Matthew Kenny <matthewkenny@gmail.com>
2012-12-18 21:42:46 -0800

    Added Dat Map

---
+++ static/js/group_mapcode.js
@@ -0,0 +1,13 @@
+/**
+ * Created with PyCharm.
+ * User: matt
+ * Date: 12/18/12
+ * Time: 9:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+}).addTo(map);
