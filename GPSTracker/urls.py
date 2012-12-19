7ccd1266c2c029abaa4e302a4e7d278c98c91420
Matthew Kenny <matthewkenny@gmail.com>
2012-12-18 21:23:33 -0800

    Implemented draft GeoJSON service.

--- GPSTracker/urls.py
+++ GPSTracker/urls.py
@@ -1,5 +1,5 @@
 __author__ = 'matt'
-from GPSTracker.models import Client, Group, Report
+from GPSTracker.models import Client, Group, Report, Point, Line, Poly
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('GPSTracker.views',
@@ -13,4 +13,16 @@ urlpatterns = patterns('GPSTracker.views',
     url(r'^groups/$', 'group'),
     # Group Detail View
     url(r'^groups/(?P<group_id>\d+)/$', 'group_detail'),
+    # Point GeoJSON View
+    url(r'^geojson/point/(?P<feat_id>\d+)/$', 'geojson'),
+    # Line GeoJSON View
+    url(r'^geojson/line/(?P<feat_id>\d+)/$', 'geojson'),
+    # Poly GeoJSON View
+    url(r'^geojson/poly/(?P<feat_id>\d+)/$', 'geojson'),
+    # Group Point GeoJSON View
+    url(r'^geojson/point/group/(?P<feat_id>\d+)/$', 'geojson_group'),
+    # Group Line GeoJSON View
+    url(r'^geojson/line/group/(?P<feat_id>\d+)/$', 'geojson_group'),
+    # Group Poly GeoJSON View
+    url(r'^geojson/poly/group/(?P<feat_id>\d+)/$', 'geojson_group'),
 )
--- GPSTracker/views.py
+++ GPSTracker/views.py
@@ -1,8 +1,12 @@
 # Create your views here.
+#from vectorformats.Formats import Django, GeoJSON
 from GPSTracker.models import Client, Group, Report, Point, Line, Poly
 from django.http import HttpResponse
 from django.template import Context, RequestContext, loader
 from django.shortcuts import render_to_response
+
+# Project Shortcuts
+from shortcuts import djangoToGeoJSON
 
 def index(request):
     return render_to_response('GPSTracker/index.html',{},context_instance=RequestContext(request))
@@ -29,3 +33,40 @@ def group_detail(request, group_id):
     line_list = Line.objects.filter(group__pk = group_id)
     poly_list = Poly.objects.filter(group__pk = group_id)
     return render_to_response('GPSTracker/group_detail.html', {'group': group,'point_list': point_list, 'line_list': line_list, 'poly_list': poly_list}, context_instance=RequestContext(request))
+
+#def geojson(request, feat_id):
+#    """Return GeoJSON object representing requested feature."""
+#    # Split request path and grab appropriate model
+#    pathParts = request.path.split('/')
+#    modelMap = {'point':Point,'line':Line,'poly':Poly}
+#    for part in pathParts:
+#        if part in modelMap:
+#            geom_rep = modelMap[part].objects.filter(pk=feat_id)
+#    # Decode django queryset and re-encode as geojson.
+#    djf = Django.Django(geodjango="geom", properties=['name','type'])
+#    geoj = GeoJSON.GeoJSON()
+#    s = geoj.encode(djf.decode(geom_rep))
+#    return HttpResponse(s)
+
+def geojson(request, feat_id):
+    """Return GeoJSON object representing requested feature."""
+    # Split request path and grab appropriate model
+    pathParts = request.path.split('/')
+    modelMap = {'point':Point,'line':Line,'poly':Poly}
+    for part in pathParts:
+        if part in modelMap:
+            geom_rep = modelMap[part].objects.filter(pk=feat_id)
+    GeoJSON = djangoToGeoJSON(request, geom_rep)
+    return HttpResponse(GeoJSON)
+
+def geojson_group(request, feat_id):
+    """Return GeoJSON object representing requested feature."""
+    # Split request path and grab appropriate model
+#    group = Group.objects.select_related().get(pk=feat_id)
+    pathParts = request.path.split('/')
+    modelMap = {'point':Point,'line':Line,'poly':Poly}
+    for part in pathParts:
+        if part in modelMap:
+            geom_rep = modelMap[part].objects.filter(group__pk=feat_id)
+    GeoJSON = djangoToGeoJSON(request, geom_rep)
+    return HttpResponse(GeoJSON)
