0b6c7e5d7dacf1c6717d5a9c3a02c927b833ad4e
Matthew Kenny <matthewkenny@gmail.com>
2012-12-22 19:44:10 -0800

    JSLint run-through.

--- static/js/group_mapcode.js
+++ static/js/group_mapcode.js
@@ -18,18 +18,18 @@ var myStyle = {
     "opacity": 0.65
 };
 
-var map = L.map('map').setView([0,0], 1);
+var map = L.map('map').setView([0, 0], 1);
 
-var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/5df4a1ed4a8c41bd91725ad594aa6139/997/256/{z}/{x}/{y}.png'
-var cloudmadeAttrib = 'Map data &copy; 2011 OSM contributors, Style &copy; 2011 CloudMade'
+var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/5df4a1ed4a8c41bd91725ad594aa6139/997/256/{z}/{x}/{y}.png';
+var cloudmadeAttrib = 'Map data &copy; 2011 OSM contributors, Style &copy; 2011 CloudMade';
 var cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18, attribution: cloudmadeAttrib});
 
-var mapQuestAerialURL = 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png'
-var mapQuestAerialAttrib = 'Portions Courtesy NASA/JPL-CalTech, USDA FSA. Thanks MapQuest!'
+var mapQuestAerialURL = 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png';
+var mapQuestAerialAttrib = 'Portions Courtesy NASA/JPL-CalTech, USDA FSA. Thanks MapQuest!';
 var mapQuestAerial = new L.TileLayer(mapQuestAerialURL, {
     maxZoom: 18,
     attribution: mapQuestAerialAttrib,
-    subdomains: ['oatile1','oatile2','oatile3','oatile4']
+    subdomains: ['oatile1', 'oatile2', 'oatile3', 'oatile4']
 });
 map.addLayer(mapQuestAerial);
 
@@ -40,29 +40,23 @@ var polyGeoJSON = new L.GeoJSON();
 var layersControl = new L.Control.Layers(baseMaps);
 map.addControl(layersControl);
 
+function makePointLayer(data) {
+    pointGeoJSON.addData(data);
+    pointGeoJSON.addTo(map);
+}
+
+function makeLineLayer(data) {
+    lineGeoJSON.addData(data);
+    lineGeoJSON.addTo(map);
+}
+
+function makePolyLayer(data) {
+    polyGeoJSON.addData(data);
+    polyGeoJSON.addTo(map);
+}
+
 // Make call to GeoJSON Service
 // Check to see if point/line/polys exist for the given group
-if (point_success = true){
-    $.getJSON(pointGroupURL, makePointLayer);
-}
-if (line_success = true){
-    $.getJSON(lineGroupURL, makeLineLayer);
-}
-if (poly_success = true){
-    $.getJSON(polyGroupURL, makePolyLayer);
-}
-
-function makePointLayer(data){
-    pointGeoJSON.addData(data);
-    pointGeoJSON.addTo(map)
-}
-
-function makeLineLayer(data){
-    lineGeoJSON.addData(data);
-    lineGeoJSON.addTo(map)
-}
-
-function makePolyLayer(data){
-    polyGeoJSON.addData(data);
-    polyGeoJSON.addTo(map)
-}
+if (point_success === true) $.getJSON(pointGroupURL, makePointLayer);
+if (line_success === true) $.getJSON(lineGroupURL, makeLineLayer);
+if (poly_success === true) $.getJSON(polyGroupURL, makePolyLayer);
