d9b14e4317946d9f04570b70f3e6f55dd0eb2207
Matthew Kenny <matthewkenny@gmail.com>
2013-02-12 22:57:11 -0800

    Test cases for file upload.

--- GPSTracker/tests.py
+++ GPSTracker/tests.py
@@ -24,7 +24,7 @@ class ExampleTest(TestCase):
     def test_response(self):
         # Issue a GET request.
         APP_ROOT = '/gpstracker'
-        URLS = ['about','clients','groups','uploadfile']
+        URLS = ['about', 'clients', 'groups', 'uploadfile']
         for url in URLS:
             response = self.client.get(os.path.join(APP_ROOT, url+'/'))
             # Check that the response is 200 OK.
@@ -41,8 +41,28 @@ class FileUploads(TestCase):
         if os.path.isfile(os.path.join(upload_dir, 'QGIS_lines.zip')):
             os.remove(os.path.join(upload_dir,'QGIS_lines.zip'))
 
-    def test_save_zip(self):
-        # TODO: Populate with actual file object.
-        self.assertTrue(save_zip(os.environ['UPLOAD_DIR'], 'DJANGO FILE OBJECT FROM REQUEST'))
-        pass
+        self.client = Client()
 
+    def test_save_zip(self):
+        """
+        Test that the file validator is working properly.
+        """
+        # TODO Replace with environment variable to create relative path to fixture dir.
+        with open('/Users/matt/PycharmProjects/PntTrax/GPSTracker/fixtures/QGIS_Lines.zip') as goodSHP:
+            response = self.client.post('/gpstracker/uploadfile/', {'file':goodSHP})
+            self.assertEqual(response.status_code, 302)
+
+        with open('/Users/matt/PycharmProjects/PntTrax/GPSTracker/fixtures/NotAZip.shp') as notAZip:
+            response = self.client.post('/gpstracker/uploadfile/', {'file':notAZip})
+            self.assertEqual(response.status_code, 200)
+            self.assertRaises(AssertionError)
+
+        with open('/Users/matt/PycharmProjects/PntTrax/GPSTracker/fixtures/TwoSHPs.zip') as twoSHPs:
+            response = self.client.post('/gpstracker/uploadfile/', {'file':twoSHPs})
+            self.assertEqual(response.status_code, 200)
+            self.assertRaises(AssertionError)
+
+        with open('/Users/matt/PycharmProjects/PntTrax/GPSTracker/fixtures/ZipNoShp.zip') as zipNoShp:
+            response = self.client.post('/gpstracker/uploadfile/', {'file':zipNoShp})
+            self.assertEqual(response.status_code, 200)
+            self.assertRaises(AssertionError)
