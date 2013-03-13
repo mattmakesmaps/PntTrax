8d93f524dac72479aac5c9e06f0fd1a382b15e07
Matthew Kenny <matthewkenny@gmail.com>
2013-03-12 19:16:06 -0700

    Added request ContextProcessor to facilitate population of 'next' query string param for login view.

--- PntTrax/settings/base.py
+++ PntTrax/settings/base.py
@@ -125,6 +125,18 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+# Added default template context processors.
+# Added request context processor to get pass request.path
+# to login view's 'next' query string parameter.
+TEMPLATE_CONTEXT_PROCESSORS = ('django.core.context_processors.request',
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages')
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
